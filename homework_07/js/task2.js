fbec83a7ac4fb7c6586d60a1820c42a126b048b5
DemjanUA <Dimon97XL@gmail.com>
2018-02-11 21:35:39 +0200

    hm_07 task2 is ready

--- homework_07/js/task2.js
+++ homework_07/js/task2.js
@@ -1,2 +1,37 @@
 'use strict';
 
+var secretNumber = 0, prize = 0, min = 0, max = 5, massage = '', userNumber = 0, currentPrize, baseAttemptPrize = 10, prizeCof = 3, rangeCof = 2;
+const maxAttempt = 3;
+
+function getRandomNumberAtRange(min, max) {
+  return Math.floor(min + Math.random() * (max + 1 - min));
+}
+
+if (confirm('Do you want to play a game?')) {
+  do {
+    secretNumber = getRandomNumberAtRange(min, max);
+    for (var i = 3; i >= 1; i--) {
+      currentPrize = Math.floor(baseAttemptPrize / (2 ** (maxAttempt - i)));
+      massage = 
+        `Enter a number from ${min} to ${max}\n` +
+        `Attempts left: ${i}\n` +
+        `Total prize: ${prize}\n` +
+        `Possible prize on current attempt: ${currentPrize}`
+      ;
+      
+      userNumber = parseInt(prompt(massage, min));
+      if (userNumber === secretNumber) {
+        prize += currentPrize;
+
+        baseAttemptPrize *= prizeCof;
+        min *= rangeCof;
+        max *= rangeCof;
+
+        break;
+      } else continue;
+    }
+  } while (confirm('Do you want to play again?'));
+  console.log(`Thank you for a game. Your prize is: ${prize}`)
+} else {
+  console.log('You did not become a millionaire');
+}
